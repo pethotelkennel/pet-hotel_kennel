17bfe96ca84341a1cbcab676da6ab6574fe372f3
masaki kazama <103808505+voltprogram@users.noreply.github.com>
2025-12-01 11:52:09 +0900

    Add Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,23 @@
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// // https://astro.build/config
+// export default defineConfig({
+//   site: 'https://voltprogram.github.io',
+//   base: '/pet-hotel_kennel',
+// });
+
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+const isProd = import.meta.env.PROD;
+
 export default defineConfig({
-  site: 'https://voltprogram.github.io',
-  base: '/pet-hotel_kennel',
+  site: isProd
+    ? 'https://voltprogram.github.io/'
+    : 'http://localhost:4321/',
+
+  base: isProd
+    ? '/pet-hotel_kennel/'
+    : '/',  // ← ローカルでは base を空にする
 });
